5fed7eabffcd20cf792dc8d1ff7d4ff1cf8873cc
Thiago Senna <trsenna@gmail.com>
2014-08-05 19:08:58 -0300

    initial version

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * "Who am I? Why am I here? A king with no name. A borrowed castle. My subjects are mindless drones.
+ * If this is the mandate of Heaven I have been given...I fear...I fear nothing...except the tedium
+ * that it will bring!!!" ~ Meruem
+ *
+ * @package    Ambling Bellows
+ * @author     Thiago Senna <thiago@thremes.com.br>
+ * @copyright  Copyright (c) 2014, Thiago Senna
+ * @link       https://github.com/thremes/ambling-bellows
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+add_action( 'after_setup_theme', 'abellows_setup' );
+
+/**
+ * The Custom Theme Setup
+ */
+function abellows_setup()
+{
+
+}
